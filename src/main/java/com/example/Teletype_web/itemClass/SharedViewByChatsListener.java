07815307d98cad70821a997da4356fce6da1822b
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2024-06-02 11:42:36 +0300

    not big upd

--- src/main/java/com/example/Teletype_web/itemClass/SharedViewByChatsListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.teletypesha.itemClass;
-
-import java.util.ArrayList;
-
-public interface SharedViewByChatsListener {
-    void onChatListChanged(ArrayList<Chat> newChatList);
-
-    void onSelectChatChanged(Chat newSelectChat);
-}
